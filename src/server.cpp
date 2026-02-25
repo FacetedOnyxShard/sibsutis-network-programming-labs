f1506e2bb889480f22006a04e7c02a773df6f14a
hikaruvi <daniilrybkin32@gmail.com>
2026-02-25 23:00:12 +0700

    add last changes

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fstream>
+#include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <signal.h>
@@ -11,11 +13,22 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+using namespace std;
+
 int main_socket;
+FILE *file;
+pthread_mutex_t st_mutex;
+pthread_attr_t ta;
 
 void cleanup(int sig) {
   printf("\n\nЗавершение работы...\n");
+
   close(main_socket);
+  fclose(file);
+
+  pthread_attr_destroy(&ta);
+  pthread_mutex_destroy(&st_mutex);
+
   exit(0);
 }
 
@@ -25,7 +38,17 @@ void transform(const char *buf, char *dest, const int buflen) {
   snprintf(dest, buflen, "%d", tnumber);
 }
 
-void buff_work(int csid, sockaddr_in *client) {
+typedef struct {
+  int csid;
+  sockaddr_in client;
+} worker_attributes_t;
+
+void *buff_work(void *args) {
+  worker_attributes_t *attributes = (worker_attributes_t *)args;
+  int csid = attributes->csid;
+  sockaddr_in client = attributes->client;
+  delete attributes;
+
   constexpr int buflen = 256;
   char buf[buflen];
   char reply[buflen];
@@ -39,15 +62,19 @@ void buff_work(int csid, sockaddr_in *client) {
       buf[n] = '\0';
     }
 
-    printf("message: %s\n", buf);
-    printf("client: ip: %s\n", inet_ntoa(client->sin_addr));
-    printf("client: port: %d\n", ntohs(client->sin_port));
+    pthread_mutex_lock(&st_mutex);
+    fprintf(file, "message: %s\n", buf);
+    fprintf(file, "client: ip: %s\n", inet_ntoa(client.sin_addr));
+    fprintf(file, "client: port: %d\n", ntohs(client.sin_port));
+    fprintf(file, "\n\n");
 
     transform(buf, reply, buflen);
     send(csid, reply, buflen, 0);
-
-    printf("\n\n");
+    pthread_mutex_unlock(&st_mutex);
   }
+  close(csid);
+
+  return NULL;
 }
 
 void reaper(int sig) {
@@ -84,22 +111,28 @@ int main(void) {
   sockaddr_in client;
   length = sizeof(client);
   pid_t child;
+
+  string filename = "../data/client_requests.txt";
+  file = fopen(filename.c_str(), "w");
+  file = fopen(filename.c_str(), "a");
+
+  pthread_t thread;
+  pthread_attr_init(&ta);
+  pthread_attr_setdetachstate(&ta, PTHREAD_CREATE_DETACHED);
+  pthread_mutex_init(&st_mutex, NULL);
+
   for (;;) {
-    client_sock = accept(main_socket, (sockaddr *)&client, &length);
-
-    child = fork();
-
-    if (child == 0) {
-      close(main_socket);
-
-      buff_work(client_sock, &client);
-
-      close(client_sock);
-      exit(0);
+    if (file == nullptr) {
+      cleanup(0);
     }
 
-    close(client_sock);
-  }
+    client_sock = accept(main_socket, (sockaddr *)&client, &length);
 
+    worker_attributes_t *attr = new worker_attributes_t;
+    attr->csid = client_sock;
+    attr->client = client;
+
+    pthread_create(&thread, NULL, buff_work, (void *)attr);
+  }
   return 0;
 }
